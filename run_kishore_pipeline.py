645281d8664c88f407548db4d594c8ebec10a102
kishoreanekalla <kishorereddyanekalla@gmail.com>
2017-08-02 13:31:24 -0500

    node allocation change

--- run_kishore_pipeline.py
+++ run_kishore_pipeline.py
@@ -4,7 +4,7 @@ from os.path import expanduser
 
 arguments = Arguments(
     msub_arguments=[
-        "-A b1042",
+        "-A b1038 ",
         "-q pulrseq",
         "-l walltime=24:00:00,nodes=1:ppn=8",
         "-m a",
@@ -18,23 +18,48 @@ arguments = Arguments(
 
 pipeline = Pipeline(name="mypipeline", join_command_arguments=True, arguments=arguments)
 
-t1 = pipeline.create_job(name="first_task")
-t1.async_run("""
-    sleep 5
-    echo {job_name} >> sophia001
-    """)
+t0 = pipeline.create_job(name="createdir")
+t0.async_run("""
+mkdir 01_fastq
+mkdir 02_fastqc
+mkdir 03_fastqc_trimmed
+mkdir 04_alignment
+mkdir 05_quantify
+""")
 
-t2 = pipeline.create_job(name="second_task")
-t2.async_run("""
-    sleep 10
-    echo {job_name} >> helio001
-    """)
+#t1 = pipeline.create_job(name="fastqc")
+#t1.async_run("""
+#    fastqc {fastqfile} -o 01_fastqc 
+#    """)
 
-t3 = pipeline.create_job(name="third_task", dependences=[t1, t2])
-t3.async_run("""
-    sleep 1
-    echo {job_name} >> FINISHED
-    """)
+#t2 = pipeline.create_job(name="trimming")
+#t2.async_run("""
+
+ #   Java -jar Trimmomatic -----
+#    echo {job_name} >> helio001
+#    """)
+#t3 = pipeline.create_job(name="fastqc_post")
+#t3.async_run("""
+#    sleep 10
+#    echo {job_name} >> helio001
+#    """)
+
+#t4 = pipeline.create_job(name="alignment")
+#t4.async_run("""
+#    sleep 10
+#    echo {job_name} >> helio001
+#    """)
+#t5 = pipeline.create_job(name="quantify")
+#t5.async_run("""
+#    sleep 10
+#    echo {job_name} >> helio001
+#    """)
+
+#t3 = pipeline.create_job(name="summarize", dependences=[t1, t2])
+#t3.async_run("""
+#    sleep 1
+#    echo {job_name} >> FINISHED
+#    """)
     
 pipeline.save_state(expanduser("~/pipeline.json"))
 pipeline.close()
