8a25ec20faab8df7a03d28e73d087bbdcf23d499
sophliu <ssliu@u.northwestern.edu>
2017-10-03 13:56:02 -0500

    modifications for trimming and pooled runs

--- run_sophia_pipeline.py
+++ run_sophia_pipeline.py
@@ -1,16 +1,17 @@
 from datetime import datetime
-import questpipe as qp 
+import questpipe as qp
 import questpipe.illumina as qpi
-import questpipe.helpers import qph
+import questpipe.helpers as qph
 import time
-   
 
-project_name, project_id = ('160728_NB501488_0018_AHFJJVBGXY', '160728_AM')
+
+project_name, project_id = ('160728_NB501488_0018_AHFJJVBGXY', '160728_AM')           # DONE
+
 
 print("Running project {}...".format(project_name))
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-run_name = "Result_{}".format(timestamp)
+run_name = "Result_20170913_170957"
 
 arguments = qp.Arguments(
     num_processors=10,
@@ -25,17 +26,19 @@ arguments = qp.Arguments(
         "-W umask=0113",
         "-N {job_name}"],
 
-    basedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/mock_questmon2",
+    basedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/mock_trimming",
     project_id=project_id,
     project_name=project_name,
     project_dir="{basedir}/{project_name}",
 
     rundir="{project_dir}/{run_name}",
-    illumina_csv_sheet="{project_dir}/SampleSheet.csv",
+    filedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/mock_questmon2/{project_name}",
+    fileloc="{filedir}/latest",
+    illumina_csv_sheet="{filedir}/SampleSheet.csv",
 
     tophat_read_mismatches=2,
     tophat_read_edit_dist=2,
-    tophat_max_multihits=5,
+    tophat_max_multihits=20,
     tophat_transcriptome_index="/projects/p20742/anno/tophat_tx/mm10.Ens_78.cuff",  # tophat uses GFF files and it does not like the extension :-(
     tophat_bowtie_index="/projects/p20742/anno/bowtie_indexes/mm10",
     quantification_transcriptome_index="/projects/p20742/anno/Ens/mm10.Ens_78/mm10.Ens_78.cuff.gtf",
@@ -45,28 +48,29 @@ arguments = qp.Arguments(
     errdir="{rundir}/logs/",
 )
 
-with qp.Pipeline(name=name, join_command_arguments=True, arguments=arguments) as pipeline:
+with qp.Pipeline(name=run_name, join_command_arguments=True, arguments=arguments) as pipeline:
     pipeline.debug_to_filename("{rundir}/pipeline.log", create_parent_folders=True)
 
     # STEP 1: Create the folders to store the data
-    _, stdout, stderr = pipeline.run("""
-        mkdir -p "{rundir}"
-        mkdir -p "{rundir}/logs"
-        mkdir -p "{rundir}/00_fastq"
-        mkdir -p "{rundir}/01_fastqc"
-        mkdir -p "{rundir}/02_trimmed"
-        mkdir -p "{rundir}/03_fastqc"
-        mkdir -p "{rundir}/04_alignment"
-        mkdir -p "{rundir}/05_quantification"
-        mkdir -p "{rundir}/06_eda"
-    """)
+    # _, stdout, stderr = pipeline.run("""
+    #     mkdir -p "{rundir}"
+    #     mkdir -p "{rundir}/logs"
+    #     mkdir -p "{rundir}/00_fastq"
+    #     mkdir -p "{rundir}/01_fastqc"
+    #     mkdir -p "{rundir}/02_trimmed"
+    #     mkdir -p "{rundir}/03_fastqc"
+    #     mkdir -p "{rundir}/03.5_multifastqc"
+    #     mkdir -p "{rundir}/04_alignment"
+    #     mkdir -p "{rundir}/05_quantification"
+    #     mkdir -p "{rundir}/06_eda"
+    # """)
 
     # STEP 2: Create fastq files
-    t1 = pipeline.create_job(name="00_bcl2fastq")
-    t1.prepare_async_run("""
-        module load bcl2fastq/2.17.1.14
-        bcl2fastq -R {project_dir} -r {num_processors} -d {num_processors} -p {num_processors} -w {num_processors}
-        """)
+    # t1 = pipeline.create_job(name="00_bcl2fastq")
+    # t1.prepare_async_run("""
+    #     module load bcl2fastq/2.17.1.14
+    #     bcl2fastq -R {project_dir} -r {num_processors} -d {num_processors} -p {num_processors} -w {num_processors}
+    #     """)
 
     # STEP 3: Create the fastqc files from fastq
     step3_tasks = []
@@ -76,41 +80,60 @@ with qp.Pipeline(name=name, join_command_arguments=True, arguments=arguments) as
         if data["Sample_Project"] != arguments.values["project_id"]:
             continue
 
-        tasks = []
+        # tasks = []
         fastq_filenames = []
-        for line in [1, 2, 3, 4]:
-            sample_filename = "{}_S{}_L{:03}_R1_001".format(data["Sample_Name"], index+1, line)
-            fastq_filenames.append("{rundir}/02_trimmed/{sample_filename}.trimmed.fastq.gz")
-            current_t = pipeline.create_job(
-                name="01_fastqc_{sample_filename}", 
-                dependences=[t1],
-                local_arguments=Arguments(
-                    sample_id=data["Sample_ID"],
-                    sample_name=data["Sample_Name"],
-                    sample_filename=sample_filename))
+        # for line in [1, 2, 3, 4]:
+        #     sample_filename = "{}_S{}_L{:03}_R1_001".format(data["Sample_Name"], index+1, line)
+        #     fastq_filenames.append("{rundir}/02_trimmed/{sample_filename}.trimmed.fastq.gz")
 
-            current_t.async_run("""
-                module load fastqc/0.11.5
-                module load java
-                
-                cp {project_dir}/Data/Intensities/BaseCalls/{project_id}/{sample_id}/{sample_filename}.fastq.gz \
-                    {rundir}/00_fastq
-                fastqc -o {rundir}/01_fastqc {rundir}/00_fastq/{sample_filename}.fastq.gz
-                java -jar /projects/b1038/tools/Trimmomatic-0.36/trimmomatic-0.36.jar SE \
-                    -threads {num_processors} \
-                    -phred33 {rundir}/00_fastq/{sample_filename}.fastq.gz \
-                    {rundir}/02_trimmed/{sample_filename}.trimmed.fastq \
-                    TRAILING:30 MINLEN:20 
-                gzip {rundir}/02_trimmed/{sample_filename}.trimmed.fastq
-                fastqc -o {rundir}/03_fastqc {rundir}/02_trimmed/{sample_filename}.trimmed.fastq.gz
-                """)
-            tasks.append(current_t)
-        
+        #     current_t = pipeline.create_job(
+        #         name="01_fastqc_{sample_filename}",
+        #         local_arguments=qp.Arguments(
+        #             sample_id=data["Sample_ID"],
+        #             sample_name=data["Sample_Name"],
+        #             sample_filename=sample_filename))
+
+        #     current_t.async_run("""
+        #         module load fastqc/0.11.5
+        #         module load java
+        #         """)
+        #         # java -jar /projects/b1038/tools/Trimmomatic-0.36/trimmomatic-0.36.jar SE \
+        #         #     -threads {num_processors} \
+        #         #     -phred33 {fileloc}/00_fastq/{sample_filename}.fastq.gz \
+        #         #     {rundir}/02_trimmed/{sample_filename}.trimmed.fastq \
+        #         #     TRAILING:30 CROP:72 HEADCROP:15 MINLEN:36
+        #         # gzip {rundir}/02_trimmed/{sample_filename}.trimmed.fastq
+        #         # fastqc -o {rundir}/03_fastqc {rundir}/02_trimmed/{sample_filename}.trimmed.fastq.gz
+        #         # """)
+        #     tasks.append(current_t)
+
+    # multi_fastqc = pipeline.create_job(
+    #     name="03.5_multiqc_{sample_name}",
+    #     dependences=tasks,
+    #     local_arguments=qp.Arguments(
+    #         sample_name=data["Sample_Name"],
+    #         fastq_filenames=",".join(fastq_filenames)))
+
+    # multi_fastqc.async_run("""
+    #     module load python
+    #     module load multiqc/1.2
+
+    #     multiqc {rundir}/03_fastqc/ \
+    #     --interactive \
+    #     -o {rundir}/03.5_multifastqc/
+    #     """)
+
         # Run tophat
+        # tophat_t = pipeline.create_job(
+        #     name="02_tophat_{sample_name}",
+        #     dependences=tasks,
+        #     local_arguments=qp.Arguments(
+        #         sample_name=data["Sample_Name"],
+        #         fastq_filenames=",".join(fastq_filenames)))
+
         tophat_t = pipeline.create_job(
             name="02_tophat_{sample_name}",
-            dependences=tasks,
-            local_arguments=Arguments(
+            local_arguments=qp.Arguments(
                 sample_name=data["Sample_Name"],
                 fastq_filenames=",".join(fastq_filenames)))
 
@@ -121,48 +144,62 @@ with qp.Pipeline(name=name, join_command_arguments=True, arguments=arguments) as
             module load boost
             module load gcc/4.8.3
             module load python
+            module load cufflinks/2.2.1
 
-            tophat --no-novel-juncs \
-                --read-mismatches {tophat_read_mismatches} \
-                --read-edit-dist {tophat_read_edit_dist} \
-                --num-threads {num_processors} \
-                --max-multihits {tophat_max_multihits} \
-                --transcriptome-index {tophat_transcriptome_index} \
-                -o {rundir}/04_alignment/{sample_name} \
-                {tophat_bowtie_index} \
-                {fastq_filenames}
-            ln -s {rundir}/04_alignment/{sample_name}/accepted_hits.bam {rundir}/04_alignment/{sample_name}.bam
-            samtools index {rundir}/04_alignment/{sample_name}.bam
-            htseq-count -f bam -q -m intersection-nonempty \
-                -s reverse -t exon -i gene_id \
-                {rundir}/04_alignment/{sample_name}.bam \
-                {quantification_transcriptome_index} \
-                > {rundir}/04_alignment/{sample_name}.htseq.counts
+            cufflinks \
+                --multi-read-correct \
+                -g {quantification_transcriptome_index} \
+                -p {num_processors} \
+                -o {rundir}/04_alignment/{sample_name}/ \
+                {rundir}/04_alignment/{sample_name}/accepted_hits.bam
             """)
-        step3_tasks.append(tophat_t)
 
-    t4 = pipeline.create_job(
-        name="03_alignment_report",
-        dependences=step3_tasks)
-    t4.async_run("""
-        module load R/3.3.1
+            # tophat --no-novel-juncs --report-secondary-alignments \
+            # --read-mismatches {tophat_read_mismatches} \
+            # --read-edit-dist {tophat_read_edit_dist} \
+            # --num-threads {num_processors} \
+            # --max-multihits {tophat_max_multihits} \
+            # --transcriptome-index {tophat_transcriptome_index} \
+            # -o {rundir}/04_alignment/{sample_name} \
+            # {tophat_bowtie_index} \
+            # {fastq_filenames}
 
-        Rscript /projects/p20742/tools/createTophatReport.R --topHatDir={rundir}/04_alignment/ --nClus={num_processors}
-        """)
+            # ln -s {rundir}/04_alignment/{sample_name}/accepted_hits.bam {rundir}/04_alignment/{sample_name}.bam
 
-    t5 = pipeline.create_job(
-        name="04_quantification",
-        dependences=[t4])
-    t5.async_run("""
-        perl /projects/p20742/tools/makeHTseqCountsTable.pl {rundir}/04_alignment \
-            {quantification_transcriptome_index} \
-            {rundir}/05_quantification
-        rm -f {project_dir}/latest
-        ln -s {rundir} {project_dir}/latest
-        """)
+            # samtools index {rundir}/04_alignment/{sample_name}.bam
+            # samtools view -h -o {rundir}/04_alignment/{sample_name}/read_info.sam \
+            # {rundir}/04_alignment/{sample_name}/accepted_hits.bam
 
-    t1.unhold()
+            # htseq-count -f bam -q -m intersection-nonempty \
+            #     -s reverse -t exon -i gene_id \
+            #     {rundir}/04_alignment/{sample_name}.bam \
+            #     {quantification_transcriptome_index} \
+            #     > {rundir}/04_alignment/{sample_name}.htseq.counts
+            # """)
+        # step3_tasks.append(tophat_t)
+
+    # t4 = pipeline.create_job(
+    #     name="03_alignment_report",
+    #     dependences=step3_tasks)
+    # t4.async_run("""
+    #     module load R/3.3.1
+
+    #     Rscript /projects/p20742/tools/createTophatReport.R --topHatDir={rundir}/04_alignment/ --nClus={num_processors}
+    #     """)
+
+    # t5 = pipeline.create_job(
+    #     name="04_quantification",
+    #     dependences=[t4])
+    # t5.async_run("""
+    #     perl /projects/p20742/tools/makeHTseqCountsTable.pl {rundir}/04_alignment \
+    #         {quantification_transcriptome_index} \
+    #         {rundir}/05_quantification
+    #     rm -f {project_dir}/latest
+    #     ln -s {rundir} {project_dir}/latest
+    #     """)
+
+    # t1.unhold()
 
     state_filename = pipeline.save_state("{rundir}/pipeline.json")
 
-qp.wait_for_pipeline(state_filename)
+# qp.wait_for_pipeline(state_filename)
---
+++ run_sophia_pipeline_pool.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+import questpipe as qp
+import questpipe.illumina as qpi
+import questpipe.helpers as qph
+from pulrseq_pipeline_sophia import run_pipeline
+
+import sys
+
+projects = [
+    # ('160728_NB501488_0018_AHFJJVBGXY', '16q0728_AM'),           # DONE
+    # ('160730_NB501488_0019_AHCK5NBGXY', '160730_Lung'),         # DONE
+    # ('160801_NB501488_0020_AHW7KJBGXX', '160801_AM'),           # DONE
+    # ('160803_NB501488_0022_AHFWHLBGXY', '160730_Lung_rerun'),   # DONE
+    # ('160804_NB501488_0023_AHFY22BGXY', '160730_AT'),           # DONE
+    # ('160805_NB501488_0024_AHFY5VBGXY', '160730_AT_rerun'),     # DONE
+    # ('160811_NB501488_0025_AHFY2GBGXY', '160811_Blood'),        # DONE
+    # ('160817_NB501488_0026_AHHL7FBGXY', '160812_MoDC'),         # DONE
+    # ('160826_NB501488_0029_AHGH72BGXY', '160826_Brain'),        # DONE
+    # ('160902_NB501488_0030_AHG7JMBGXY', '160902_Brain'),        # DONE
+    # ('160903_NB501488_0031_AHHCYFBGXY', '160903_Cerebellum'),   # DONE
+    # ('160909_NB501488_0033_AHG723BGXY', '160909_Heart'),        # DONE
+    # ('160922_NB501488_0038_AHLMWCBGXY', '160922_Kidney'),       # DONE
+    # ('161025_NB501488_0047_AHVLGJBGXY', '161025_GutEP'),        # DONE
+    # ('170323_NB501488_0080_AHVV5GBGXY', '170323_Liver'),        # DONE
+    # ('170329_NB501488_0082_AH2VV5BGX2', '170329_Adrenal'),      # DONE
+    # ('170330_NB501488_0083_AHFJY7BGX2', '170330_Esophagus'),    # DONE
+    # ('170331_NB501488_0084_AHM2LJBGX2', '170331_Stomach'),      # DONE
+    # ('170405_NB501488_0086_AHMG5YBGX2', '170405_DB_ATAM'),      # DONE
+    # ('170412_NB501488_0092_AHLYWVBGX2', '170410_SI'),           # DONE
+    # ('170413_NB501488_0093_AH5KGFBGX2', '170411_LI'),           # DONE
+    # ('170414_NB501488_0094_AH5FYKBGX2', '170412_BAT'),          # DONE
+    # ('170420_NB501488_0097_AHFFHNBGX2', '170422_WAT'),          # DONE
+    # ('170421_NB501488_0098_AHFG5MBGX2', '170423_DB_WL'),        # DONE
+    # ('170424_NB501488_0099_AHCLK7BGX2', '170424_LI'),           # DONE
+    # ('170531_NB501488_0114_AHT2FVBGX2', '170531_Skin'),         # DONE
+    # ('170717_NB501488_0130_AH5T2CBGX3', '170717_MuscSat'),      # DONE
+    # ('170727_NB501488_0135_AHFF5VBGX3', '170727_MuscSat'),
+    # ('170731_NB501488_0136_AHFGKTBGX3', '170731_MuscSat'),
+    # ('170921_NB501488_0147_AHN7CFBGX3', '170921_Brain')]
+    ('171003_AM', '171003_AM')]
+
+from_index = 0
+to_index = len(projects)
+
+if len(sys.argv) > 1:
+    from_index = int(sys.argv[1])
+
+if len(sys.argv) > 2:
+    to_index = int(sys.argv[2])
+
+
+MAX_PIPELINES = 4
+waiting_list = []
+for project_name, project_id in projects[from_index:to_index]:
+    print("Running project {}...".format(project_name))
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    run_name = "Result_{}".format(timestamp)
+    run_name = 'Result_20171003_132013'
+
+    # arguments = qp.Arguments(
+    #     num_processors=10,
+    #     run_name=run_name,
+
+    #     msub_arguments=[
+    #         "-A b1042",
+    #         "-q pulrseq",
+    #         "-l walltime=24:00:00,nodes=1:ppn={num_processors}",
+    #         "-m a",
+    #         "-j oe",
+    #         "-W umask=0113",
+    #         "-N {job_name}"],
+
+    #     basedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/lane_splitting",
+    #     project_id=project_id,
+    #     project_name=project_name,
+    #     project_dir="{basedir}/{project_name}",
+
+    #     rundir="{project_dir}/{run_name}",
+    #     filedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/lane_splitting/{project_name}",
+    #     fileloc="{filedir}/latest",
+    #     illumina_csv_sheet="{filedir}/SampleSheet.csv",
+
+    #     tophat_read_mismatches=2,
+    #     tophat_read_edit_dist=2,
+    #     tophat_max_multihits=5,
+    #     tophat_transcriptome_index="/projects/p20742/anno/tophat_tx/mm10.Ens_78.cuff",  # tophat uses GFF files and it does not like the extension :-(
+    #     tophat_bowtie_index="/projects/p20742/anno/bowtie_indexes/mm10",
+    #     quantification_transcriptome_index="/projects/p20742/anno/Ens/mm10.Ens_78/mm10.Ens_78.cuff.gtf",
+
+    #     workdir="{project_dir}/",
+    #     outdir="{rundir}/logs/",
+    #     errdir="{rundir}/logs/",
+    # )
+
+    arguments = qp.Arguments(
+        num_processors=10,
+        run_name=run_name,
+
+        msub_arguments=[
+            "-A b1042",
+            "-q pulrseq",
+            "-l walltime=24:00:00,nodes=1:ppn={num_processors}",
+            "-m a",
+            "-j oe",
+            "-W umask=0113",
+            "-N {job_name}"],
+
+        basedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/pooling_runs",
+        project_id=project_id,
+        project_name=project_name,
+        project_dir="{basedir}/{project_name}",
+
+        rundir="{project_dir}/{run_name}",
+        filedir="/projects/b1038/Pulmonary/Workspace/mouse_aging_map/users/general/pooling_runs/{project_name}",
+        fileloc="{filedir}/latest",
+        illumina_csv_sheet="{filedir}/SampleSheet.csv",
+
+        tophat_read_mismatches=2,
+        tophat_read_edit_dist=2,
+        tophat_max_multihits=5,
+        tophat_transcriptome_index="/projects/p20742/anno/tophat_tx/mm10.Ens_78.cuff",  # tophat uses GFF files and it does not like the extension :-(
+        tophat_bowtie_index="/projects/p20742/anno/bowtie_indexes/mm10",
+        quantification_transcriptome_index="/projects/p20742/anno/Ens/mm10.Ens_78/mm10.Ens_78.cuff.gtf",
+
+        workdir="{project_dir}/",
+        outdir="{rundir}/logs/",
+        errdir="{rundir}/logs/",
+    )
+
+    state_filename = run_pipeline(run_name, arguments=arguments)
+    waiting_list.append(state_filename)
+    while len(waiting_list) >= MAX_PIPELINES:
+        current = waiting_list.pop(0)
+        print("Waiting for project {}...".format(current))
+        qph.wait_for_pipeline(current)
+
+while len(waiting_list) > 0:
+    current = waiting_list.pop(0)
+    print("Waiting for project {}...".format(current))
+    qph.wait_for_pipeline(current)
